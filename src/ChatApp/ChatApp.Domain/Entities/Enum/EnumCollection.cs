35de989c77e30941c7db2d07bc8600ff66695563
Shariar Alfaz <77987580+Shariar-Alfaz@users.noreply.github.com>
2023-11-12 00:29:24 +0600

    identity added

---
+++ src/ChatApp/ChatApp.Domain/Entities/Enum/EnumCollection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Domain.Entities.Enum
+{
+    public class EnumCollection
+    {
+        public enum Gender
+        {
+            Male,
+            Female,
+            Other
+        }
+    }
+}
---
+++ src/ChatApp/ChatApp.Persistence/Membership/ApplicationUserClaim.cs
@@ -0,0 +1,6 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ChatApp.Persistence.Membership
+{
+    public class ApplicationUserClaim : IdentityUserClaim<Guid> { }
+}
